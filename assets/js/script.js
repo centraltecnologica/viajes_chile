c2a206ed7ebc0151e2720d561ad2bba4be0722e3
vlarap <vlarap@centraltecnologica.cl>
2024-02-25 19:40:48 -0300

    Se corrige y cuadran las Card de la seccion destacados

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
-// Tooltip enabling
+// Habilitar Tooltip
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+//SmootScroll
+const dataSpyList = document.querySelectorAll('[data-bs-spy="scroll"]')
+dataSpyList.forEach(dataSpyEl => {
+  bootstrap.ScrollSpy.getInstance(dataSpyEl).refresh()
+})
